1d2f1976f9a2baa751b2f6de12583c8740499416
lz <406602937@qq,com>
2019-12-13 17:41:51 +0800

    feat:Color Collect Pro页面 - 01

---
+++ lib/base/style.dart
@@ -0,0 +1,6 @@
+
+import 'package:flutter/material.dart';
+
+getCommonTextStyle() {
+  return TextStyle(fontSize: 14, color: Colors.black);
+}
--- lib/page/purchase_page.dart
+++ lib/page/purchase_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app2/base/base.dart';
-import 'package:flutter_app2/base/base_page.dart';
-import 'package:flutter_app2/dialog/dialog_router.dart';
-import 'package:flutter_app2/dialog/transparent_dialog.dart';
-import 'package:flutter_app2/dialog/test_dialog.dart';
+import 'package:flutter_app2/base/style.dart';
 
 class PurchasePage extends StatefulWidget {
   @override
@@ -24,39 +21,111 @@ class _PurchasePage extends BaseState<PurchasePage> {
       appBar: BaseAppBar(
         backgroundColor: Colors.white,
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              GestureDetector(
-                onTap: () {
-                  print("click");
-                },
-                child: Padding(
-                  padding: EdgeInsets.all(25),
-                  child: Text("Close", style: TextStyle(fontSize: 14, color: Colors.black)),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  print("click2");
-//                  showMyDialog(
-//                      context: context,
-//                      builder: (BuildContext context) {
-//                        return TestDialog();
-//                      });
+          _getTopButton(),
+          _getTitleContent(),
+          _getPriceInfo(),
+        ],
+      ),
+    );
+  }
 
-                  Navigator.push(context, DialogRouter(TestDialog()));
-                },
-                child: Padding(
-                  padding: EdgeInsets.all(25),
-                  child: Text("Restore", style: TextStyle(fontSize: 14, color: Colors.black)),
-                ),
+  ///获取顶部按钮
+  _getTopButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            print("click");
+          },
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Text("Close", style: TextStyle(fontSize: 14, color: Colors.black)),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            print("click2");
+          },
+          child: Padding(
+            padding: EdgeInsets.all(15),
+            child: Text("Restore", style: getCommonTextStyle()),
+          ),
+        ),
+      ],
+    );
+  }
+
+  ///获取标题和内容
+  _getTitleContent() {
+    return Container(
+      color: Colors.red,
+      margin: EdgeInsets.fromLTRB(20, 25, 20, 20),
+      child: Column(
+        children: <Widget>[
+          Text(
+            "Color Collect Pro",
+            style: TextStyle(fontSize: 20, color: Colors.black, fontWeight: FontWeight.w600),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Text(
+              "Get yourself a color companion with the price of a cup of your morning coffee.",
+              style: getCommonTextStyle(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  ///获取价格信息
+  _getPriceInfo() {
+//    return  Column(
+//      crossAxisAlignment: CrossAxisAlignment.start,
+//      children: <Widget>[
+//        Container(
+//          child: Text(
+//            "Lifetime - pay once, unlock forever",
+//            style: getCommonTextStyle(),
+//          ),
+//        ),
+//        Container(
+//          child: Text(
+//            "￥25.00",
+//            style: getCommonTextStyle(),
+//          ),
+//        ),
+//      ],
+//    );
+
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+//      color: Colors.red,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage("http://file02.16sucai.com/d/file/2014/0704/e53c868ee9e8e7b28c424b56afe2066d.jpg"),
+          fit: BoxFit.cover
+        )
+      ),
+      child: Stack(
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+
+              Text(
+                "Lifetime - pay once, unlock forever",
+                style: getCommonTextStyle(),
+              ),
+              Text(
+                "￥25.00",
+                style: getCommonTextStyle(),
               ),
             ],
-          ),
-          Text("123sdfjo1ijoi23jfio23foin23oinfo2i3nfo2n3oin2oi3nfo32nio2nf23no2f2onfo2no2no2nfo"),
+          )
         ],
       ),
     );
